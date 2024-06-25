bbc3d6538e12ca4619666fb74450e66597092681
md-azmi-siddique <md.azmi.siddique@gmail.com>
2024-06-26 05:24:23 +0600

    ProductSliderModel

--- backend/api/models.py
+++ backend/api/models.py
@@ -51,3 +51,15 @@ class ProductsDetailsModel(models.Model):
     productID = models.ForeignKey(ProductsModel, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True) 
+    
+    def __str__(self):
+        return self.productID
+    
+class ProductSliderModel(models.Model):
+    title = models.CharField(max_length=255)
+    des = models.CharField(max_length=255)
+    price = models.CharField(max_length=255)
+    img = models.CharField(max_length=255)
+    productID = models.ForeignKey(ProductsModel, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True) 
